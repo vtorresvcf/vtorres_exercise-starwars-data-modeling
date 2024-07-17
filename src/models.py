e2af15935f81d23e1a144f63bcf3734284545519
Vicente Torres Luna <143291208+vtorresvcf@users.noreply.github.com>
2024-07-17 14:37:44 +0000

    finish

--- src/models.py
+++ src/models.py
@@ -1,32 +1,51 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, func, DateTime
+from sqlalchemy_utils import EmailType, PasswordType
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+#Instalado adicionalmente passlib y sqlalchemy_utils para implementar password y EmailType
+#Importado adicionalmente func para calcular la fecha al momento actual
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(50),nullable=False)
+    last_name = Column(String(70))
+    email = Column(EmailType,nullable=False)
+    password = Column(PasswordType(schemes=['pbkdf2_sha512']), nullable=False)
+    date_inscription = Column(DateTime, default=func.now(), onupdate=func.now(), nullable=False)
+    favorites = relationship("favorite", back_populates="user")
 
-    def to_dict(self):
-        return {}
+
+class Characters(Base):
+    __tablename__ = 'characters'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50),nullable=False)
+    heigth = Column(Integer,nullable=False)
+    mass = Column(Integer,nullable=False)
+    hair_color = Column(String(50),nullable=False)
+
+class Planets(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50),nullable=False)
+    population = Column(Integer,nullable=False)
+    rotation_period = Column(Integer,nullable=False)
+    orbital_period = Column(Integer,nullable=False)
+    diameter = Column(Integer,nullable=False)
+
+class Favorite(Base):
+    __tablename__ = "favorite"
+    id = Column(Integer, primary_key=True)
+    id_user = Column(Integer, ForeignKey("user.id"))
+    id_characters = Column(Integer, ForeignKey("characters.id"))
+    id_planets = Column(Integer, ForeignKey("planets.id"))
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
